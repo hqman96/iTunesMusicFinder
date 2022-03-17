b5c4fbcc2d57f7dc420dacd81b9561154cd68213
Georgiy Kiselev <hqman96@gmail.com>
2022-03-17 13:07:14 +0300

    added foundAlbums array and textDidChange method of albumSearchBar changed numberOfRowsInSection and cellForRowAt methods

--- iTunesMusicFinder/MainViewController.swift
+++ iTunesMusicFinder/MainViewController.swift
@@ -10,28 +10,46 @@ import UIKit
 class MainViewController: UIViewController {
 
     var albums = ["album 1","album 2","album 3","album 4"]
-    
+    var foundAlbums: [String]!
+   
     @IBOutlet weak var albumSearchBar: UISearchBar!
     @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        albumSearchBar.delegate = self
         mainTableView.delegate = self
         mainTableView.dataSource = self
+        foundAlbums = albums
     }
 }
 
-extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+extension MainViewController: UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        foundAlbums = []
+        if searchText == "" {
+            foundAlbums = albums
+        }
+        else {
+            for album in albums {
+                if album.lowercased().contains(searchText.lowercased()) {
+                    foundAlbums.append(album)
+                }
+            }
+            self.mainTableView.reloadData()
+        }
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return albums.count
+        return foundAlbums.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-        cell.textLabel?.text = albums[indexPath.row]
+        cell.textLabel?.text = foundAlbums[indexPath.row]
         return cell
     }
 }
