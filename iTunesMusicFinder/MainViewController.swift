d4191853e9822e10f76a9eee03f36fd82340debd
Georgiy Kiselev <hqman96@gmail.com>
2022-03-18 21:30:55 +0300

    Added url to request albums in iTunes Added searchTask variable to query using URLSession Changed data type of the albums array to [Album], made the array empty, added a setter with reloading the array data using DispatchQueue.main.async Removed foundAlbums array and changed numberOfRowsInSection and cellForRowAt methods

--- iTunesMusicFinder/MainViewController.swift
+++ iTunesMusicFinder/MainViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    var albums = ["album 1","album 2","album 3","album 4"]
-    var foundAlbums: [String]!
+    let url =  "https://itunes.apple.com/search?entity=album&offset=0&limit=100&term="
+    var albums: [Album] = [] {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.mainTableView.reloadData()
+            }
+        }
+    }
+    var searchTask: URLSessionDataTask?
     
     @IBOutlet weak var albumSearchBar: UISearchBar!
     @IBOutlet weak var mainTableView: UITableView!
@@ -20,25 +27,36 @@ class MainViewController: UIViewController {
         albumSearchBar.delegate = self
         mainTableView.delegate = self
         mainTableView.dataSource = self
-        foundAlbums = albums
     }
 }
 
 extension MainViewController: UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        foundAlbums = []
         if searchText == "" {
-            foundAlbums = albums
+            albums = []
         }
         else {
-            for album in albums {
-                if album.lowercased().contains(searchText.lowercased()) {
-                    foundAlbums.append(album)
+            if let urlString = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                if let searchUrl = URL(string: "\(url)\(urlString)") {
+                    searchTask?.cancel()
+                    searchTask = URLSession.shared.dataTask(with: searchUrl) { data, response, error in
+                        if let data = data {
+                            let decoder = JSONDecoder()
+                            do {
+                                let dataModel = try decoder.decode(AlbumDataModel.self, from: data)
+                                self.albums = dataModel.results
+                            }
+                            catch {
+                                print("error")
+                            }
+                        }
+                    }
+                    searchTask?.resume()
                 }
             }
-            self.mainTableView.reloadData()
         }
+        self.mainTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -51,11 +69,11 @@ extension MainViewController: UISearchBarDelegate, UITableViewDelegate, UITableV
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foundAlbums.count
+        return albums.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-        cell.textLabel?.text = foundAlbums[indexPath.row]
+        cell.textLabel?.text = albums[indexPath.row].albumName
         return cell
     }
 }
